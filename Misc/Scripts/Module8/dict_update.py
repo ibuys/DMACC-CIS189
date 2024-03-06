ffe19a4893d7e2214510269631ff54182cd0a933
jtriv <james.triveri@gmail.com>
2024-03-06 13:34:13 -0600

    JDT: Module 8.

---
+++ Misc/Scripts/Module8/dict_update.py
@@ -0,0 +1,52 @@
+"""
+CIS189 Module 8 Dictionary Update Assignment.
+"""
+
+
+
+def main():
+    """
+    Create a phone book for the names and numbers in the given lists.
+    The follow the instructions in the comments. 
+    """
+
+    names = ["juan", "michael", "steve", "jimmy", "tony", "merle", "willie"]
+
+    numbers = ["708-555-9878", "630-555-0009", "630-555-1112", "515-555-9999", 
+               "847-555-4443", "312-555-9823", "414-555-6767"]
+    
+    # --------------------------------------------------------------------------
+    # TODO: Create an empty dictionary. Iterate over names and numbers, 
+    # creating a dictionary that maps names to numbers. 
+    # Hint: Look into to using thew zip function to iterate over names 
+    # and numbers as a list of 2-tuples.
+    #---------------------------------------------------------------------------
+    
+
+
+    #---------------------------------------------------------------------------
+    # TODO: Prompt user for a name NOT in your dictionary (continue to prompt 
+    # user until they enter a name not already in the dictionary). Once a new 
+    # name is provided, prompt for a phone number. Then add the key-value pair 
+    # to your dictionary.
+    #---------------------------------------------------------------------------
+
+   
+
+    #---------------------------------------------------------------------------
+    # TODO: Prompt user for a name in your dictionary (continue to prompt 
+    # user until they enter a name already in the dictionary). Once an existing
+    # name is provided, prompt for a new phone number, and update the dict
+    # with the new number. 
+    #---------------------------------------------------------------------------
+   
+
+   
+    #---------------------------------------------------------------------------
+    # TODO: Print dictionary with new name/number and updated number. 
+    # --------------------------------------------------------------------------
+            
+
+
+if __name__ == "__main__":
+    main()
