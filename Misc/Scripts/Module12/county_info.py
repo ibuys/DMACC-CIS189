3b81abb68787d271b31b0319f76c289be8f795a3
jtriv <james.triveri@gmail.com>
2024-04-05 14:10:53 -0500

    JDT: Added Module-12.ipynb.

---
+++ Misc/Scripts/Module12/county_info.py
@@ -0,0 +1,45 @@
+
+"""
+CIS189 Module 12 CSV Import to Class Object Assignment script.
+
+Remove all instances of pass before running/submitting your code.
+Submitted files with pass included will receive 0 points.
+"""
+
+class County:
+
+    def __init__(self):
+        
+        # Add the required arguments to the constructor  and attributes to the 
+        # County class, one attribute for each column in the dataset. 
+
+        ##### YOUR CODE HERE #####
+        pass
+
+
+    def get_population(self):
+        """
+        Return the population as a float.
+        """
+
+        ##### YOUR CODE HERE #####
+        pass
+
+
+
+
+
+def main():
+    # Use the DictReader object to read in the CSV file. Each row will be represented
+    # as a dictionary, with the column name as the key and the associated value 
+    # the value. 
+    # Create a County object for each county. 
+    # Finally, print the total population for all counties in Iowa by by calling
+    # get_population for each, and maintaining a running total. 
+    pass
+
+
+
+if __name__ == "__main__":
+
+    main()
